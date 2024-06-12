206b3790221f6d774623bcecf493abeb1af5faad
Nerimantas7 <nerimantas.tarabilda@stud.techin.lt>
2024-06-12 09:20:26 +0300

    Modifications V3

--- src/main/java/lt/nerimantas/book_recommendation_web_app/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package lt.nerimantas.book_recommendation_web_app.repository;
-
-import lt.nerimantas.book_recommendation_web_app.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
