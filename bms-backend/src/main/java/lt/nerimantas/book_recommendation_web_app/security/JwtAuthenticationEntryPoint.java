8d266e9f70f7311aacf7433773fd7ca78516d1df
Nerimantas7 <nerimantas.tarabilda@stud.techin.lt>
2024-09-06 15:54:11 +0300

    Adding JWT token to secure REST APIs

---
+++ bms-backend/src/main/java/lt/nerimantas/book_recommendation_web_app/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,24 @@
+package lt.nerimantas.book_recommendation_web_app.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    // this comments method is called whenever an exception is thrown due to an unauthorized user trying to access a resource that requires an authentication.
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+
+    }
+}
